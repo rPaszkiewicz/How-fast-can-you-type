1107f38dbf95fcc08d85817c4899caedf87f7159
Dev Mąż <yakuzatanaka@gmail.com>
2019-04-20 22:32:36 +0200

    StoredWords and driver added

---
+++ src/main/java/entity/StoredWords.java
@@ -0,0 +1,29 @@
+package entity;
+
+import java.util.ArrayList;
+
+public class StoredWords{
+
+    private ArrayList<String> list;
+
+    public StoredWords(){
+        this.list = new ArrayList<>();
+        list.add("shit");
+        list.add("happends");
+        list.add("bubble");
+    }
+
+    public void print(){
+        for (String s : list)
+            System.out.print(s + ", ");
+    }
+
+    public ArrayList<String> getWords(){
+        return list;
+    }
+
+    public void remove(String word){
+        list.remove(word);
+    }
+
+}
